ca22b2084d62a3c8e34b1f2ef3a8b567d9ede0dc
codeForever033 <chatt.abhi033@gmail.com>
2021-06-03 23:17:30 +0530

    commit -m 'final commit'

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import TechSelectOptions from '../techs/TechSelectOptions';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -10,7 +14,16 @@ const AddLogModal = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, tech, attention);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date(),
+      };
+
+      addLog(newLog);
+
+      M.toast({ html: `Log added by ${tech}` });
 
       // clear fields
       setMessage('');
@@ -48,9 +61,7 @@ const AddLogModal = () => {
               <option value='' disabled>
                 Select Technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Sara Wilson'>Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
@@ -84,10 +95,12 @@ const AddLogModal = () => {
     </div>
   );
 };
-
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired,
+};
 const modalStyle = {
   width: '75%',
   height: '75%',
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
